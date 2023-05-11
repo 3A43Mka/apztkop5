8160e06496c262d35d263d5c76b09134125753c0
unknown <3a43mka@gmail.com>
2023-05-11 22:28:29 +0300

    Signatures, tests

---
+++ 1-layered-architecture/db_layer.py
@@ -0,0 +1,19 @@
+class DatabaseLayer:
+
+    def __init__(self):
+        self.items = []
+
+    def get_all_items(self):
+        pass
+
+    def add_item(self, new_item):
+        pass
+
+    def update_item(self, updated_item):
+        pass
+
+    def delete_item(self, id):
+        pass
+
+    def get_item_by_id(self, id):
+        pass
