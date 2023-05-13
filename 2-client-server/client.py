cc578ce79f796a7a80b8d2c5d414a7b58af674d4
3A43Mka <3a43mka@gmail.com>
2023-05-13 19:30:49 +0300

    Server and client implementation

--- 2-client-server/client.py
+++ 2-client-server/client.py
@@ -4,4 +4,15 @@ import socket
 class Client:
 
     def __init__(self):
-        pass
+        self.client_socket = None
+
+    def connect(self, host, port):
+        self.client_socket = socket.socket()
+        self.client_socket.connect((host, port))
+
+    def send_message(self, msg):
+        self.client_socket.send(msg.encode())
+        return self.client_socket.recv(1024).decode()
+
+    def close_connection(self):
+        self.client_socket.close()
