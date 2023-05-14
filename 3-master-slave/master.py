4e4541f676cbd11961c9df69eb00a7b184016c61
3A43Mka <3a43mka@gmail.com>
2023-05-14 20:36:27 +0300

    Slave and master implementations

--- 3-master-slave/master.py
+++ 3-master-slave/master.py
@@ -1,7 +1,26 @@
 class Master:
     def __init__(self):
-        pass
+        self.results = []
+        self.logs = []
+        self.slaves = []
+
+    def add_slave(self, slave):
+        self.slaves.append(slave)
 
     def process(self):
-        pass
+        while len(self.slaves):
+            for slave in self.slaves:
+                if slave.is_complete():
+                    res = slave.get_result()
+                    self.logs.append(res)
+                    self.results.append(res)
+                    self.slaves.remove(slave)
+                else:
+                    res = slave.do_task()
+                    self.logs.append(res)
 
+    def get_logs(self):
+        return self.logs
+
+    def get_results(self):
+        return self.results
--- 3-master-slave/slave.py
+++ 3-master-slave/slave.py
@@ -1,6 +1,20 @@
 class Slave:
-    def __init__(self):
-        pass
+    def __init__(self, task_text, steps):
+        self.task_text = task_text
+        self.steps = steps
 
     def do_task(self):
-        pass
+        if self.steps > 0:
+            self.steps -= 1
+            return f'Doing task "{self.task_text}", {self.steps} steps left.'
+        return None
+
+    def is_complete(self):
+        if self.steps == 0:
+            return True
+        return False
+
+    def get_result(self):
+        if self.steps == 0:
+            return f'Result for the task "{self.task_text}".'
+        return None
