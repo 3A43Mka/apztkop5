9401d84dd41906b4cb737e76471838ae37dca7fc
3A43Mka <3a43mka@gmail.com>
2023-05-14 16:49:08 +0300

    Tests fix, main

--- 2-client-server/main.py
+++ 2-client-server/main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from server import Server
+from client import Client
+import socket
+from threading import Thread
 
 
-# Press the green button in the gutter to run the script.
+def run_server():
+    s = Server()
+    s.bind_and_listen(" and here is the hi from server!", socket.gethostname(), 5000)
+
+
+def run_client():
+    c = Client()
+    c.connect(socket.gethostname(), 5000)
+    print(c.send_message("Hi 1 from client"))
+    print(c.send_message("Hi 2 from client!"))
+    c.close_connection()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    p1 = Thread(target=run_server)
+    p1.start()
+    p2 = Thread(target=run_client)
+    p2.start()
--- 2-client-server/test_main.py
+++ 2-client-server/test_main.py
@@ -1,11 +1,29 @@
+from server import Server
+from client import Client
+import socket
+from threading import Thread
+
+response1 = None
+response2 = None
+
 
 def run_server():
-    pass
+    s = Server()  # Press Ctrl+F8 to toggle the breakpoint.
+    s.bind_and_listen(" and here is the hi from server!", socket.gethostname(), 5000)
 
 
 def run_client():
-    pass
+    c = Client()
+    c.connect(socket.gethostname(), 5000)
+    response1 = c.send_message("Hi 1 from client")
+    response2 = c.send_message("Hi 2 from client!")
+    assert response1 == "Hi 1 from client and here is the hi from server!"
+    assert response2 == "Hi 2 from client! and here is the hi from server!"
+    c.close_connection()
 
 
 def test_send_message():
-    pass
+    p1 = Thread(target=run_server)
+    p1.start()
+    p2 = Thread(target=run_client)
+    p2.start()
