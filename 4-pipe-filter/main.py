29f052f598532167b0178f9ecc3a85e016f54350
3A43Mka <3a43mka@gmail.com>
2023-05-15 19:24:05 +0300

    Implementation, main app

--- 4-pipe-filter/main.py
+++ 4-pipe-filter/main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from pipeline import Pipeline
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def add_bun(hamburger):
+    hamburger.append('bun')
 
 
-# Press the green button in the gutter to run the script.
+def add_cheese(hamburger):
+    hamburger.append('cheese')
+
+
+def add_sauce(hamburger):
+    hamburger.append('sauce')
+
+
+def add_ham(hamburger):
+    hamburger.append('ham')
+
+
+def main():
+    pipeline = Pipeline()
+    hamburger = []
+    pipeline.add([add_bun, add_sauce, add_ham, add_cheese, add_bun])
+    pipeline.execute(hamburger)
+    print(hamburger)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
--- 4-pipe-filter/pipeline.py
+++ 4-pipe-filter/pipeline.py
@@ -1,6 +1,12 @@
 class Pipeline:
     def __init__(self):
-        pass
+        self.filters = list()
+
+    def add(self, filter):
+        self.filters.extend(filter)
 
     def execute(self, message):
-        pass
+        print("Executing pipeline...")
+        for message_filter in self.filters:
+            message_filter(message)
+
