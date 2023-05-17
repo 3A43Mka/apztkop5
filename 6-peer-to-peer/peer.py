91196833985c37816a01a7a8a017c122a5c99c24
3A43Mka <3a43mka@gmail.com>
2023-05-18 01:32:55 +0300

    Initial, tests

---
+++ 6-peer-to-peer/peer.py
@@ -0,0 +1,15 @@
+class Peer:
+    def __init__(self, initial_data):
+        pass
+
+    def connect(self, other):
+        pass
+
+    def disconnect(self):
+        pass
+
+    def send_data(self, data):
+        pass
+
+    def get_data(self):
+        pass
