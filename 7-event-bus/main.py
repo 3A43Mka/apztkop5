ff3eaf16688dd2df51562b4ca70074aaf46f4a3a
3A43Mka <3a43mka@gmail.com>
2023-05-20 21:03:21 +0300

    Fixes, main app scenario

--- 7-event-bus/main.py
+++ 7-event-bus/main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from event_bus import EventBus
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def sausage_cutter(pizza):
+    pizza['sausages'] = True
 
 
-# Press the green button in the gutter to run the script.
+def pizza_packer(pizza):
+    pizza['is_in_the_box'] = True
+
+
+def icing_on_the_cake_putter(pizza):
+    pizza['icing'] = True
+
+
+def delivery_man(args):
+    print(f'Delivery Man: went out to deliver the pizza')
+
+
+PIZZA_IS_READY = 'pizza_is_ready'
+PIZZA_IS_BEING_PREPARED = 'pizza_is_being_prepared'
+DELIVER_PIZZA = 'deliver_pizza'
+
+
+def main():
+    bus = EventBus()
+    # subscribing event listeners to events
+    bus.add_event_listener(PIZZA_IS_BEING_PREPARED, sausage_cutter)
+    bus.add_event_listener(PIZZA_IS_READY, pizza_packer)
+    bus.add_event_listener(PIZZA_IS_READY, icing_on_the_cake_putter)
+    bus.add_event_listener(DELIVER_PIZZA, delivery_man)
+
+    # now emitting events
+
+    new_pizza = {}
+    bus.emit(PIZZA_IS_BEING_PREPARED, new_pizza)
+    print(f'Pizza should now have only sausages')
+    print(new_pizza)
+    bus.emit(PIZZA_IS_READY, new_pizza)
+    print(f'Pizza now should have icing and be packed')
+    print(new_pizza)
+    bus.emit(DELIVER_PIZZA)
+    print(f'Delivery man should now be on route to deliver pizza')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
