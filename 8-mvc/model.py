bcea20f687be59d0e72224ce9cca4cd372bacebb
3A43Mka <3a43mka@gmail.com>
2023-05-21 20:24:58 +0300

    Model implemented, tests

--- 8-mvc/model.py
+++ 8-mvc/model.py
@@ -1,18 +1,33 @@
 class Model:
     def __init__(self):
-        pass
+        self.users = []
+        self.next_id = 1
 
     def get_all_users(self):
-        pass
+        return self.users
 
     def get_user_by_id(self, id):
-        pass
+        for user in self.users:
+            if user['id'] == id:
+                return user
+        return None
 
     def add_user(self, new_user):
-        pass
+        new_user['id'] = self.next_id
+        self.next_id += 1
+        self.users.append(new_user)
+        return new_user
 
     def update_user(self, updated_user):
-        pass
+        for i in range(len(self.users)):
+            if self.users[i]['id'] == updated_user['id']:
+                self.users[i] = updated_user
+                return self.users[i]
+        return None
 
     def delete_user(self, id):
-        pass
+        for user in self.users:
+            if user['id'] == id:
+                self.users.remove(user)
+                return user
+        return None
